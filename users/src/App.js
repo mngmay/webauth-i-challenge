7eca44fb99e41a71fa3f217d9fe5ddab2ade9779
May Ng <mngmay@gmail.com>
2019-09-17 14:15:29 -0700

    logout functionality works on app

--- users/src/App.js
+++ users/src/App.js
@@ -3,10 +3,12 @@ import { Route } from "react-router-dom";
 import PrivateRoute from "./utils/PrivateRoute.js";
 import LoginForm from "./components/LoginForm";
 import UserList from "./components/UserList";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Route path="/" component={Nav} />
       <Route exact path="/" component={LoginForm} />
       <PrivateRoute path="/users" component={UserList} />
     </div>
